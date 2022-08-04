331a59bc341f186026e26199207008bdaeb36c68
FidaaMilhem <109407100+FidaaMilhem@users.noreply.github.com>
2022-08-04 05:21:15 +0300

    q1

---
+++ lesson-01/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace lesson_01
+{
+    internal class Program
+    {
+
+
+        static void print(double[] list)
+        {
+            if (list is null)
+            {
+                Console.WriteLine("Nothing");
+                return;
+            }
+
+            Console.Write("[ ");
+
+            if (list.Length > 0)
+            {
+                Console.Write($"{list[0]}");
+            }
+
+            for (int i = 1; i < list.Length; i++)
+            {
+                Console.Write($", {list[i]}");
+            }
+            Console.WriteLine(" ]");
+        }
+        static void Main(string[] args)
+        {
+          
+            double[] numbers = { 1.1,3.0,5.2 };
+            
+
+            print(numbers);
+            
+
+
+
+
+            Console.WriteLine("Fin");
+        }
+    }
+}
