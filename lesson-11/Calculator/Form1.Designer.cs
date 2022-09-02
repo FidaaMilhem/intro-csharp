083b27b9f84c2ea40603d03fc10ca64b8d8aa78e
FidaaMilhem <109407100+FidaaMilhem@users.noreply.github.com>
2022-09-02 23:36:08 +0300

    lesson 11 -Calculator

---
+++ lesson-11/Calculator/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Calculator
+{
+    partial class CounterForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCounter = new System.Windows.Forms.Label();
+            this.btnUp = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lblError = new System.Windows.Forms.Label();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btn_multi = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblCounter
+            // 
+            this.lblCounter.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.lblCounter.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCounter.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblCounter.ForeColor = System.Drawing.Color.Coral;
+            this.lblCounter.Location = new System.Drawing.Point(64, 49);
+            this.lblCounter.Name = "lblCounter";
+            this.lblCounter.Size = new System.Drawing.Size(200, 33);
+            this.lblCounter.TabIndex = 0;
+            this.lblCounter.Text = "-1";
+            this.lblCounter.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnUp
+            // 
+            this.btnUp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnUp.Location = new System.Drawing.Point(12, 112);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(110, 43);
+            this.btnUp.TabIndex = 1;
+            this.btnUp.Text = "Up";
+            this.btnUp.UseVisualStyleBackColor = false;
+            this.btnUp.Click += new System.EventHandler(this.OnButtonnUp);
+            // 
+            // btnDown
+            // 
+            this.btnDown.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnDown.Location = new System.Drawing.Point(212, 112);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(110, 43);
+            this.btnDown.TabIndex = 2;
+            this.btnDown.Text = "Down";
+            this.btnDown.UseVisualStyleBackColor = false;
+            this.btnDown.Click += new System.EventHandler(this.OnButtonDown);
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnReset.Location = new System.Drawing.Point(12, 206);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(110, 43);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.OnButtonReset);
+            // 
+            // txtInput
+            // 
+            this.txtInput.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.txtInput.Location = new System.Drawing.Point(140, 217);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(182, 23);
+            this.txtInput.TabIndex = 4;
+            // 
+            // lblError
+            // 
+            this.lblError.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblError.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblError.ForeColor = System.Drawing.Color.Red;
+            this.lblError.Location = new System.Drawing.Point(12, 430);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(310, 50);
+            this.lblError.TabIndex = 5;
+            this.lblError.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(334, 25);
+            this.toolStrip1.TabIndex = 6;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Items.AddRange(new object[] {
+            "5",
+            "10",
+            "12",
+            "15",
+            "19",
+            "20"});
+            this.listBox1.Location = new System.Drawing.Point(217, 263);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(91, 139);
+            this.listBox1.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button1.ForeColor = System.Drawing.Color.Blue;
+            this.button1.Location = new System.Drawing.Point(33, 272);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 56);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_add);
+            // 
+            // btn_multi
+            // 
+            this.btn_multi.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btn_multi.ForeColor = System.Drawing.Color.Blue;
+            this.btn_multi.Location = new System.Drawing.Point(33, 334);
+            this.btn_multi.Name = "btn_multi";
+            this.btn_multi.Size = new System.Drawing.Size(122, 56);
+            this.btn_multi.TabIndex = 9;
+            this.btn_multi.Text = "multibly";
+            this.btn_multi.UseVisualStyleBackColor = false;
+            this.btn_multi.Click += new System.EventHandler(this.btn_multi_Click);
+            // 
+            // CounterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.ClientSize = new System.Drawing.Size(334, 487);
+            this.Controls.Add(this.btn_multi);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnDown);
+            this.Controls.Add(this.btnUp);
+            this.Controls.Add(this.lblCounter);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "CounterForm";
+            this.Text = "Noobus Kalkullator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label lblCounter;
+        private Button btnUp;
+        private Button btnDown;
+        private Button btnReset;
+        private TextBox txtInput;
+        private Label lblError;
+        private ToolStrip toolStrip1;
+        private ListBox listBox1;
+        private Button button1;
+        private Button btn_multi;
+    }
+}
---
+++ lesson-11/Calculator/Form1.cs
@@ -0,0 +1,100 @@
+namespace Calculator
+{
+    public partial class CounterForm : Form
+    {
+        public int Value { get; set; }
+
+        public CounterForm()
+        {            
+            Value = 0;            
+            InitializeComponent();
+            lblError.BorderStyle = BorderStyle.None;
+            lblCounter.Text = Value.ToString();
+        }
+
+        private void UpdateDisplay()
+        {
+            lblCounter.Text = Value.ToString();
+        }
+
+        private void OnButtonnUp(object sender, EventArgs e)
+        {
+            lblError.Text = "";
+            Value += 1;
+            UpdateDisplay();
+
+            if(listBox1.Items.Count > 0)
+            {
+                lblError.Text = listBox1.Items[0].ToString();
+            }
+        }
+
+        private void OnButtonDown(object sender, EventArgs e)
+        {
+            lblError.Text = "";
+            Value -= 1;
+            UpdateDisplay();
+            //listBox1.Items.Add(13);
+            //listBox1.Items.Add(27);
+        }
+
+        private void OnButtonReset(object sender, EventArgs e)
+        {
+            var t = txtInput.Text;
+
+            int n;
+            if( int.TryParse(t, out n))
+            {
+                Value = n;
+                UpdateDisplay();
+                lblError.Text = "";
+            }
+            else
+            {
+                lblError.Text = $"Not a Number: {t}";
+            }
+
+            txtInput.Text = "";
+        }
+
+        private void button1_add(object sender, EventArgs e)
+        {
+            
+            double sum = 0;
+            int c = listBox1.Items.Count;
+            if (listBox1.Items.Count > 0)
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    sum += Convert.ToDouble(listBox1.Items[c-1-i]);
+
+                    //if (i == c - 1)
+                    //{
+                    //    listBox1.Items.RemoveAt(c);
+                    //}   
+                }
+                listBox1.Items.RemoveAt(c - 1);
+            }
+            
+            lblCounter.Text = sum.ToString();
+        }
+
+        private void btn_multi_Click(object sender, EventArgs e)
+        {
+            double mul = 1;
+            int c = listBox1.Items.Count;
+            if (listBox1.Items.Count > 0)
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    mul *= Convert.ToDouble(listBox1.Items[c - 1 - i]);
+    
+                }
+
+            }
+            listBox1.Items.RemoveAt(c - 1);
+            lblCounter.Text = mul.ToString();
+
+        }
+    }
+}
