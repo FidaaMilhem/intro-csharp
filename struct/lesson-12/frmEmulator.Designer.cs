adf83f80bbfe27c8f44926c2fed5baed9bd87dbd
FidaaMilhem <109407100+FidaaMilhem@users.noreply.github.com>
2022-09-08 21:49:48 +0300

    design of form Emulator

---
+++ struct/lesson-12/frmEmulator.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace lesson_12
+{
+    partial class Emulator
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnStep = new System.Windows.Forms.Button();
+            this.Run = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.textBox1.Location = new System.Drawing.Point(324, 600);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(258, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(324, 636);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(258, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(354, 49);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(135, 514);
+            this.listBox1.TabIndex = 2;
+            // 
+            // btnStep
+            // 
+            this.btnStep.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btnStep.Location = new System.Drawing.Point(602, 608);
+            this.btnStep.Name = "btnStep";
+            this.btnStep.Size = new System.Drawing.Size(102, 24);
+            this.btnStep.TabIndex = 3;
+            this.btnStep.Text = "Step";
+            this.btnStep.UseVisualStyleBackColor = false;
+            // 
+            // Run
+            // 
+            this.Run.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.Run.Location = new System.Drawing.Point(735, 608);
+            this.Run.Name = "Run";
+            this.Run.Size = new System.Drawing.Size(102, 24);
+            this.Run.TabIndex = 4;
+            this.Run.Text = "btnRun";
+            this.Run.UseVisualStyleBackColor = false;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 15;
+            this.listBox2.Location = new System.Drawing.Point(569, 49);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(135, 334);
+            this.listBox2.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(66, 49);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(226, 583);
+            this.textBox2.TabIndex = 6;
+            // 
+            // Emulator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1114, 659);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.Run);
+            this.Controls.Add(this.btnStep);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Emulator";
+            this.Text = "Emulator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private ListBox listBox1;
+        private Button btnStep;
+        private Button Run;
+        private ListBox listBox2;
+        private TextBox textBox2;
+    }
+}
---
+++ struct/lesson-12/frmEmulator.cs
@@ -0,0 +1,12 @@
+namespace lesson_12
+{
+    public partial class Emulator : Form
+    {
+        public Emulator()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
