6c187da64cdb4a1d8db4059cc6b6d6e28f5441a4
FidaaMilhem <109407100+FidaaMilhem@users.noreply.github.com>
2022-09-10 19:14:01 +0300

    PUSHIP/POPIP/DROPIP

---
+++ lesson-12-Emulator/Form_MachineEmulator.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Emulator
+{
+    public partial class Form_MachineEmulator : Form
+    {        
+        private ProgramExecuter _executor;        
+
+        private readonly DataStack _stack;
+        private readonly DataStack _stackIP;
+
+        public Form_MachineEmulator()
+        {
+            InitializeComponent();
+            _stack = new DataStack(stackViewerPush, stackViewerPop);
+            _stackIP = new DataStack(IPstackViewerPush, IPstackViewerPop);
+            textBox_ProgramCode.Text = DemoPrograms.SimpleWithJumpsIP;
+        }
+
+        private void BuildCode_Click(object sender, EventArgs e)
+        {           
+            var compiler = new Compiler();
+            string sourceCode = textBox_ProgramCode.Text.Trim();
+            var opcodes = compiler.BuildCode(sourceCode);
+
+            listBox_ExeCode.Items.Clear();
+            int label = 0;
+            foreach (var opcode in opcodes)
+            {
+                listBox_ExeCode.Items.Add($"{label++}: {opcode.ToString()}");
+            }
+
+            _executor = new ProgramExecuter(opcodes, _stack,_stackIP);
+
+            label_PC.Text = "PC: 0";
+            listBox_StackViewer.Items.Clear();
+        }
+
+        private  void stackViewerPush(int data)
+        {
+            listBox_StackViewer.Items.Add(data);
+        }
+        private  void stackViewerPop()
+        {
+            listBox_StackViewer.Items.RemoveAt(listBox_StackViewer.Items.Count - 1);
+        }
+        private void IPstackViewerPush(int data)
+        {
+            listBox_IpStack.Items.Add(data);
+        }
+        private void IPstackViewerPop()
+        {
+            listBox_IpStack.Items.RemoveAt(listBox_IpStack.Items.Count - 1);
+        }
+        private void ExecuteStep_Click(object sender, EventArgs e)
+        {
+            if (_executor.IsHalted)
+            {
+                textBox_ExecutingMessage.Text = "STOPED";                
+            }
+            else
+            {
+                
+                if (_executor.ExecuteStep())
+                {
+                    textBox_ExecutingMessage.Text = "Executing";
+                }
+                else
+                {
+                    textBox_ExecutingMessage.Text = "Halting on Error";
+                }
+                
+
+                label_PC.Text = $"PC: {_executor._controller.PC}";
+            }
+            
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            textBox_ProgramCode.Clear();
+            listBox_StackViewer.Items.Clear();
+            listBox_ExeCode.Items.Clear();
+            listBox_IpStack.Items.Clear();
+        }
+
+        private void Run_Click(object sender, EventArgs e)
+        {
+            while(!_executor.IsHalted)
+            {
+                btn_ExecuteStep.PerformClick();
+            }
+            textBox_ExecutingMessage.Text = "STOPED";
+        }
+
+        private void listBox_StackViewer_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox_IpStack_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
