a7d51522ec27a18e12e40df0ed35155fdb007f24
FidaaMilhem <109407100+FidaaMilhem@users.noreply.github.com>
2022-09-26 14:03:23 +0300

    Chat Session/Application

---
+++ lesson-20/WebEmulator/WebEmulator/Chat.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Chat.aspx.cs" Inherits="Chat" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:ListBox ID="lstChat" runat="server" Height="260px" Width="1009px"></asp:ListBox>
+        </div>
+        <p>
+            <asp:TextBox ID="txtChat" runat="server" Height="55px" Width="568px"></asp:TextBox>
+            <asp:Button ID="Button1" runat="server" Height="36px" Text="SEND" Width="100px" OnClick="Button1_Click" />
+        </p>
+        <p>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;
+        </p>
+        <p>
+            &nbsp;</p>
+    </form>
+</body>
+</html>
---
+++ lesson-20/WebEmulator/WebEmulator/Chat.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Chat : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //if (Session["lstChat"] != null)
+        //{
+        //   lstChat.Items.Clear();
+        //   var items =(ListItemCollection) Session["lstChat"];
+        //    foreach(ListItem item in items)
+        //        lstChat.Items.Add(item);   
+        //}
+        if (Application["lstChat"] != null)
+        {
+            lstChat.Items.Clear();
+            var items = (ListItemCollection)Application["lstChat"];
+            foreach (ListItem item in items)
+                lstChat.Items.Add(item);
+        }
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        lstChat.Items.Add(txtChat.Text);
+        Session["lstChat"] = lstChat.Items;
+        Application["lstChat"] = lstChat.Items;
+        txtChat.Text = "";
+    }
+
+
+
+   
+}
